6929397728275cf2b799941dfbe777585a7dc481
vicky <birendrapradhan112@gmail.com>
2025-11-14 23:58:03 +0530

    change base api url and added build

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const config = {
-    API_BASE_URL: "http://localhost/bravodent_ci",
+    // API_BASE_URL: "http://localhost/bravodent_ci",
+    API_BASE_URL: "https://fsp.bravodentdesigns.com",
+
 }
 
 
