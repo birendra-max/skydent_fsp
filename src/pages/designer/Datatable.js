858853721a39db58654911ef3233483d852f68bf
vicky <birendrapradhan112@gmail.com>
2025-12-19 20:19:22 +0530

    added designer id in runSelf function for update in progress status

--- src/pages/designer/Datatable.js
+++ src/pages/designer/Datatable.js
@@ -230,8 +230,16 @@ export default function Datatable({
                 if (!record) continue;
 
                 if (record.status === 'New' || record.status === 'Redesign') {
-                    await fetchWithAuth(`/update-pending/${id}`);
-                    successCount++;
+                    const resp = await fetchWithAuth(`/update-pending`, {
+                        method: "POST",
+                        body: JSON.stringify({ desi_id: designer.desiid, orderid: id })
+                    });
+                    if (resp.status === 'success') {
+                        successCount++;
+                    }
+                    else {
+                        invalidCount++;
+                    }
                 } else {
                     invalidCount++;
                 }
