3c9f3f65e0ebd8380f277ae2c71e49d817cd1e26
vicky <birendrapradhan112@gmail.com>
2026-01-04 21:39:13 +0530

    added new url in backend server

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     API_BASE_URL: "http://localhost/bravodent_ci",
-    // API_BASE_URL: "https://fsp.bravodentdesigns.com",
+    // API_BASE_URL: "https://dentalserver.online",
 
 }
 
